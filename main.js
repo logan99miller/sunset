6bd821dbb4825525185c77a5288b84692d528afb
logan <logan99miller@gmail.com>
2017-11-12 09:02:03 +0000

    version 0.0.2
    Added support to click through the window and interact with other programs. Also added comments and removed some surplus code.

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-const {app, BrowserWindow} = require("electron")
-const path = require("path")
-const url = require("url")
-let win
+//allows use of shorter terms to refer to x
+const {app, BrowserWindow} = require("electron"); 
+const path = require("path");
+const url = require("url");
+
+let win //global reference of window to prevent program shutting down when JS garbage is collected
+
 function createWindow () {
-    //win.setIgnoreMouseEvents(ignore) = new BrowserWindow ({
         win = new BrowserWindow ({
+        
+        //all required for transparency 
         transparent: true,
         frame: false,
         toolbar: false,
         fullscreen: true
 
-    })
+    });
+
+    win.setIgnoreMouseEvents(true); //allows user to click on windows / type without being blocked by window created by program
+    win.setAlwaysOnTop(true, "screen"); //prevents another program to be brought to front of our window
+    
+    //loads file containing a opaque and coloured div
     win.loadURL(url.format({
-        pathname: path.join(__dirname, "index.html"),
+        pathname: path.join(__dirname, "index.html"), //allows files to be moved about
         protocol: "file",
         slashes: true
-    }))
+    }));
 }
-app.on("ready", createWindow)
+app.on("ready", createWindow);
